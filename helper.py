765c233179610ab5dbf765a1a8738ca685929b59
olakoya <ola.koya1@gmail.com>
2024-09-28 21:43:27 +0100

    updates

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
-
+import os
+#self.driver = webdriver.Chrome(os.environ.get('CHROMEDRIVER_PATH'))
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -6,32 +7,42 @@ from selenium.webdriver.firefox import options
 from selenium.webdriver.firefox.options import Options
 from time import sleep
 
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+
+#search_field = WebDriverWait(self.driver, 10).until(
+ #   EC.presence_of_element_located((By.ID, 'twotabsearchtextbox'))
+#)
+
+
 class Automation:
     def __init__(self, browser, url, search):
         self.browser = browser
         self.url = url
         self.search = search
 
-        if self.browser == "Chrome":
-            self.driver = webdriver.Chrome(r'C:\chromedriver.exe')
+        if self.browser == "Chrome": 
+            # Use environment variable for ChromeDriver path
+            self.driver = webdriver.Chrome(os.environ.get('CHROMEDRIVER_PATH')) #(r'C:\chromedriver.exe')
         elif self.browser == "Firefox":
             option = Options()
-            option.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-            self.driver = webdriver.Firefox(options=option, executable_path=r'C:\geckodriver.exe')
+            # Use environment variable for Firefox binary location
+            option.binary_location = os.environ.get('FIREFOX_BINARY_PATH', r'C:\Program Files\Mozilla Firefox\firefox.exe')#r'C:\Program Files\Mozilla Firefox\firefox.exe'
+            self.driver = webdriver.Firefox(options=option, executable_path=os.environ.get('GECKODRIVER_PATH', r'C:\geckodriver.exe'))#r'C:\geckodriver.exe')
         else:
             print('Browser not found')
 
-    def open_browser(self): 
-        if self.browser == "Chrome":
-            self.driver.get(self.url)
-        elif self.browser == "Firefox":
-            self.driver.get(self.url)
-        else:
-            print('Browser not found')
+    def open_browser(self):
+        self.driver.get(self.url) 
+        #if self.browser == "Chrome":
+         #   self.driver.get(self.url)
+        #elif self.browser == "Firefox":
+         #   self.driver.get(self.url)
+        #else:
+        #    print('Browser not found')
 
     def search_string(self):
         if 'geeksforgeeks' in self.url:
-            
             self.driver.find_element(By.CSS_SELECTOR, '.gfg-icon_search').click()
             search_field = self.driver.find_element(By.ID, 'gcse-search-input')
             search_field.send_keys(self.search)
@@ -46,12 +57,13 @@ class Automation:
             print('Search string not found')
 
     def close_browser(self):
-        if self.browser == 'Chrome':
-            self.driver.close()
-        elif self.browser == 'Firefox':
-            self.driver.close()
-        else:
-            print('Browser not found')
+        self.driver.close()
+       # if self.browser == 'Chrome':
+       #     self.driver.close()
+       # elif self.browser == 'Firefox':
+       #     self.driver.close()
+        #else:
+       #     print('Browser not found')
 
 
 class AmazonShopping(Automation):
@@ -81,9 +93,11 @@ class AmazonShopping(Automation):
                 pass
 
     def add_to_cart(self):
-        self.driver.find_element(By.CSS_SELECTOR, '#submit\.add-to-cart').click()
+        self.driver.find_element(By.CSS_SELECTOR, r'#submit.add-to-cart').click()
         sleep(10)
 
+
+print("Script is running!")
 
 # website = Automation('Firefox', 'https://www.facebook.com', 'apple')
 
